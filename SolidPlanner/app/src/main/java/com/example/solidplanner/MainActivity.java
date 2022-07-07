c724ec2479e0ec9c8779253f985306fc2efcea3a
michelle.alanis <michelle.alanis@u-erre.mx>
2022-07-07 15:36:14 -0500

    Sprint 1: Iniciar Sesión y Crear Nuevo Usuario

---
+++ SolidPlanner/app/src/main/java/com/example/solidplanner/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.solidplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void InlogIn(View view){
+        Intent i = new Intent(this, LogInActivity.class);
+        startActivity(i);
+    }
+
+    public void InsignIn(View view){
+        Intent i = new Intent(this, SignInActivity.class);
+        startActivity(i);
+    }
+
+}
